540618df57978dc17855729a467ada833e5ca4bf
ChristianMichelsen <christianmichelsen@gmail.com>
2019-04-03 18:51:29 +0200

    Add reading of BAM file to LCA_test.py

--- src/LCA_test.py
+++ src/LCA_test.py
@@ -8,6 +8,7 @@ Created on Wed Apr  3 16:06:04 2019
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 
 file_LCA = '../data/raw/Mex_9_S50_L005_R1_001_holi_LCA.lca'
@@ -67,10 +68,36 @@ df = pd.DataFrame.from_dict(res)
 
 # remove bad rows
 df = df.loc[df['phylo_type'] != '-1']
+# sort by N
+df = df.sort_values('N', ascending=False)
+
+N_phylos = 100
+# get the N_pylos most observed phylos
+phylo_int_top = (df.groupby('phylo_int')['N']
+                   .sum()
+                   .sort_values(ascending=False)
+                   .iloc[:N_phylos]
+                 )
+
+# get the reads from the original df that are in the top10 most observed phylo
+# and sort the dataframe
+df_top = df.loc[df['phylo_int'].isin(phylo_int_top.index)].sort_values('N', ascending=False)
+
+set_names_top = df_top.groupby('phylo_int')['name'].apply(set).to_dict()
+
+name_to_phylo_int = {}
+for key, vals in set_names_top.items():
+    for val in vals:
+        name_to_phylo_int[val] = key
+
+phylo_int_to_str = {x: f'{z}:{y}' for x,y,z in 
+                    df_top[['phylo_int', 'phylo_name', 'phylo_type']].values}
+
 
 
 
 qname = 'D00708:299:CB6VWANXX:5:2307:8147:26950'
+# qname2 = ':'.join(qname.split(':')[:-1])
 
 
 _BAM_UNMAPPED  = 0x4
@@ -96,18 +123,23 @@ import pysam
 # open SAM/BAM file
 in_bam = pysam.AlignmentFile(file_bam_in)
 
-it = _read_bamfile(in_bam)
+N_reads = 619956074
+
+it = tqdm(_read_bamfile(in_bam), total=N_reads)
             
 # main loop
-for counter, read_bam in enumerate(it, start=1):
-    # counter += 1
-    
-    if read_bam.qname == qname:
-        assert False
-        
-        
-        
 
+reads_matching = {}
+
+for counter, read_bam in enumerate(it, start=1):
+    
+    phylo_int = name_to_phylo_int.get(read_bam.qname)
+    
+    if phylo_int is not None:
+        # print(f"Got one at counter={counter}")
+        reads_matching.setdefault(phylo_int, []).append(read_bam)
+    
+    
 
 
 
